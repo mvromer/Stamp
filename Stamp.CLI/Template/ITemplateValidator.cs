d74a077a51b01edb59e0df6f2a3e742f69f62a65
Michael Romer <mvromer@gmail.com>
2019-05-12 22:52:43 -0500

    Added template name validation

---
+++ Stamp.CLI/Template/ITemplateValidator.cs
@@ -0,0 +1,7 @@
+namespace Stamp.CLI.Template
+{
+    interface ITemplateValidator
+    {
+        void ValidateName( string templateName );
+    }
+}
--- Stamp.CLI/Template/TemplateDirectoryValidator.cs
+++ Stamp.CLI/Template/TemplateDirectoryValidator.cs
@@ -5,15 +5,25 @@ namespace Stamp.CLI.Template
 {
     class TemplateDirectoryValidator : ITemplateDirectoryValidator
     {
-        internal TemplateDirectoryValidator( IFileSystem fileSystem )
+        internal TemplateDirectoryValidator( IFileSystem fileSystem,
+            ITemplateFactory templateFactory,
+            ITemplateValidator templateValidator )
         {
             this.FileSystem = fileSystem;
+            this.TemplateFactory = templateFactory;
+            this.TemplateValidator = templateValidator;
         }
 
         public void Validate( IPurePath templateDir )
         {
+            var manifestPath = templateDir.WithFilename( TemplateConstants.ManifestFileName );
+            var template = this.TemplateFactory.CreateFromManifest( manifestPath );
+
+            this.TemplateValidator.ValidateName( template.Name );
         }
 
         private IFileSystem FileSystem { get; }
+        private ITemplateFactory TemplateFactory { get; }
+        private ITemplateValidator TemplateValidator { get; }
     }
 }
