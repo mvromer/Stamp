0cb024f0674209ae8419f8ce4eb2e95c04c1d231
Michael Romer <mvromer@gmail.com>
2020-05-31 23:18:25 -0500

    Baby attempt at adding some comments

--- Stamp.CLI/Template/ITemplateLoader.cs
+++ Stamp.CLI/Template/ITemplateLoader.cs
@@ -6,8 +6,23 @@ namespace Stamp.CLI.Template
 {
     interface ITemplateLoader
     {
+        /// <summary>
+        /// Loads a template from its manifest stored at the given path.
+        /// </summary>
+        /// <param name="manifestPath">Path to the manifest file for the template to load.</param>
+        /// <returns>
+        /// A template object defined by the contents of the given manifest.
+        /// </returns>
         ITemplate LoadFromManifest( IPurePath manifestPath );
 
+        /// <summary>
+        /// Loads a template from its manifest currently opened by the given <c>TextReader</c>.
+        /// </summary>
+        /// <param name="reader"><c>TextReader</c> opened to the manifest of the template to load.</param>
+        /// <returns>
+        /// A template object defined by the contents of manifest opened by the given
+        /// <c>TextReader</c>.
+        /// </returns>
         ITemplate LoadFromReader( TextReader reader );
 
         ITemplate FindTemplate( string templateName );
